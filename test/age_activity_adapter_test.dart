90e72be6157c22f3e7bd8dd7a9138fa1bcb26345
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-17 17:18:32 +0100

    fix(test): import AgeBand for activity adaptation tests

--- test/age_activity_adapter_test.dart
+++ test/age_activity_adapter_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:arqami/core/profile/age_activity_adapter.dart';
+import 'package:arqami/core/profile/learner_profile.dart';
 import 'package:arqami/models/unit_model.dart';
 
 void main() {
