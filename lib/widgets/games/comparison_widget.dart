1908f9f94d3d4b5ee5f4f27ecfc9dcb457f61592
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-18 02:03:42 +0100

    fix(games): restore comparison theme import

--- lib/widgets/games/comparison_widget.dart
+++ lib/widgets/games/comparison_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../core/theme/app_colors.dart';
-import '../../core/theme/game_theme.dart;
+import '../../core/theme/game_theme.dart';
 
 /// نوع السؤال: أيهما أكثر، أيهما أقل، أو هل هما متساويان.
 enum ComparisonQuestion {
