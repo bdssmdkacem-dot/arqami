c08abd44e85ec83deb25f4aa0f18bbc02d27be69
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-18 17:11:21 +0100

    fix(test): import curriculum activity planner

--- test/curriculum_test.dart
+++ test/curriculum_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:arqami/core/profile/learner_profile.dart';
 import 'package:arqami/core/profile/age_activity_adapter.dart';
+import 'package:arqami/models/curriculum_spec.dart';
 import 'package:arqami/models/number_path.dart';
 import 'package:arqami/models/unit_model.dart';
 import 'package:arqami/models/units_data.dart';
@@ -294,7 +295,6 @@ void main() {
         );
         expect(adapted, isNotEmpty, reason: '${unit.id} / $band');
         expect(adapted.last, isA<AssessmentActivityConfig>(), reason: '${unit.id} / $band');
-        expect(adapted.every((activity) => activity is ActivityConfig), isTrue);
       }
     }
   });
